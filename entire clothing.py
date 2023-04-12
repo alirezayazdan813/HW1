e842abaea18fb7ba953dc7f87b2f0ceeb0024393
Alirezayazdan <alirezayazdan813@gmail.com>
2023-04-12 14:50:48 +0430

    coding child classes for clothing and testing by instances

--- entire clothing.py
+++ entire clothing.py
@@ -1,4 +1,3 @@
-# Clothing class with aggregation and composition
 class Clothing:
     def __init__(self, size, color, brand, price, Zip, Pock, Butt, Lin, Embell):
         self.size = size
@@ -16,44 +15,46 @@ class Clothing:
         self.thread = Thread()
 
     def get_zipper(self):
-        return self.zipper.get_zip()
-    
+        return self.zipper.get_zip(self)
+
     def get_pocket(self):
-        return self.pocket.get_pock()
-    
+        return self.pocket.put_item(self)
+
     def get_button(self):
-        return self.button.get_butt()
-    
+        return self.buttons.add_button(self)
+
     def get_lining(self):
-        return self.lining.get_lin()
-    
+        return self.lining.change_lining(self)
+
     def get_embellishment(self):
-        return self.embellishments.get_embell()
-    
+        return self.embellishments.add_embellishment(self)
+
     def wash(self):
         print("Washing instructions: Machine wash cold, tumble dry low")
-    
+
     def iron(self):
         print("Ironing instructions: Iron on low heat")
 
+
 # Aggregation classes
 class Zipper:
-    def __init__(self, ziptype : bool):
+    def __init__(self, ziptype: bool):
         self.ziptype = ziptype
         self.length = 10
         self.color = 'silver'
 
     def zip_up(self):
         print("Zipper is now zipped up")
-    
+
     def get_zip(self):
         if self.ziptype == True:
-            print("The clothing have zipper")
-        else :
-            print("The clothing does not have zipper")
+            print("The clothing has a zipper")
+        else:
+            print("The clothing does not have a zipper")
+
 
 class Pocket:
-    def __init__(self, pocktype : bool):
+    def __init__(self, pocktype: bool):
         self.pocktype = pocktype
         self.number_of_pockets = 2
         self.size = 'medium'
@@ -61,29 +62,20 @@ class Pocket:
     def put_item(self):
         print("Item has been put in pocket")
 
-    def get_pock(self):
-        if self.pocktype == True:
-            print("The clothing has pocket")
-        else :
-            print("The clothing does not have pocket")
 
 class Buttons:
-    def __init__(self, butttype : bool):
+    def __init__(self, butttype: bool):
         self.butttype = butttype
         self.number_of_buttons = 5
         self.color = 'black'
 
     def add_button(self):
         print("A new button has been added")
-    
-    def get_butt(self):
-        if self.butttype == True:
-            print("The clothing has button")
-        else :
-            print("The clothing does not have button")
+
+
 
 class Lining:
-    def __init__(self, lintype : bool):
+    def __init__(self, lintype: bool):
         self.lintype = lintype
         self.type = 'satin'
         self.color = 'white'
@@ -93,27 +85,16 @@ class Lining:
         self.color = new_color
         print(f"Lining has been changed to {self.color} {self.type}")
 
-    def get_lin(self):
-        if self.lintype == True:
-            print("The clothing has lining")
-        else :
-            print("The clothing does not have lining")
 
 
 class Embellishments:
-    def __init__(self, embelltype : bool):
+    def __init__(self, embelltype: bool):
         self.embelltype = embelltype
         self.type = 'beads'
         self.color = 'gold'
 
     def add_embellishment(self):
-        print("An embellishment has been added")   
-
-    def get_embell(self):
-        if self.embelltype == True:
-            print("The clothing has embellishment")
-        else :
-            print("The clothing does not have embellishment")
+        print("An embellishment has been added")
 
        
 # Composition classes
@@ -131,8 +112,73 @@ class Thread:
         self.color = 'white'
         self.type = 'polyester'
 
-    def change_thread(self, new_color, new_type):
+    def change_color(self, new_color):
         self.color = new_color
+        print(f"Thread has been changed to {self.color} {self.type}")
+
+    def change_type(self, new_type):
         self.type = new_type
         print(f"Thread has been changed to {self.color} {self.type}")
-        
+
+# Child classes with polymorphism
+class Shirt(Clothing):
+    def __init__(self, size, color, brand, price, Zip, Pock, Butt, Lin, Embell, sleeve_length):
+        super().__init__(size, color, brand, price, Zip, Pock, Butt, Lin, Embell)
+        self.sleeve_length = sleeve_length
+
+    def iron(self):
+        print("Ironing instructions: Iron on medium heat")
+
+class Jeans(Clothing):
+    def __init__(self, size, color, brand, price, Zip, Pock, Butt, Lin, Embell, style):
+        super().__init__(size, color, brand, price, Zip, Pock, Butt, Lin, Embell)
+        self.style = style
+
+    def wash(self):
+        print("Washing instructions: Machine wash in warm water, tumble dry low")
+
+class Shorts(Clothing):
+    def __init__(self, size, color, brand, price, Zip, Pock, Butt, Lin, Embell, length):
+        super().__init__(size, color, brand, price, Zip, Pock, Butt, Lin, Embell)
+        self.length = length
+
+    def wash(self):
+        print("Washing instructions: Machine wash in cold water, hang to dry")
+
+class Socks(Clothing):
+    def __init__(self, size, color, brand, price, Zip, Pock, Butt, Lin, Embell, material):
+        super().__init__(size, color, brand, price, Zip, Pock, Butt, Lin, Embell)
+        self.material = material
+
+    def wash(self):
+        print("Washing instructions: Machine wash in warm water, tumble dry low")
+
+class Jacket(Clothing):
+    def __init__(self, size, color, brand, price, Zip, ziptype, Pock, Butt, Lin, Embell, hooded):
+        super().__init__(size, color, brand, price, Zip, Pock, Butt, Lin, Embell)
+        self.hooded = hooded
+        self.ziptype = ziptype
+
+    def iron(self):
+        print("Ironing instructions: Iron on low heat")
+    
+    def hoodhood(self):
+        print("This jacket is hooded : {}".format(self.hooded))
+
+    def get_zip(self):
+        return self
+    #?????????????????????????????????????????????????????????????????????
+
+# testing inharitance and aggregation 
+mmd = Jacket("XL", "red", "sinsin", 18, Zipper, True, Pocket, Buttons, Lining, Embellishments, False)
+mmd.iron()
+mmd.hoodhood()
+mmd.get_zipper()
+
+# testing composition
+ali = Socks("medium", "white", "jj", 5, Zipper, Pocket, Buttons, Lining, Embellishments, 'cotton')
+ali.wash()
+ali.get_button()
+ali.fabric.change_color("blue")
+
+
